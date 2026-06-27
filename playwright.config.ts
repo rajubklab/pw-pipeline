9a21a946323d0ea9edcc39f4a9cebdb34c04d3a0
rajubklab <rajubk.homelab@gmail.com>
2026-06-27 10:11:25 +0530

    dfsfs

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     },
   },
   webServer: {
-    command: "pnpm dev",
+    command: "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     env: {
